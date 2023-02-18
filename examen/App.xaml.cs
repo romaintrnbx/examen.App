34e8f08370de3577fbda51fa44d6f259f6f95a48
romaintrnbx <romain.troonbeeckx@gmail.com>
2023-02-18 09:29:55 +0100

    Ajoutez des fichiers projet.

---
+++ examen/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace examen;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ examen/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace examen;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
